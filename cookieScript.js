0e04de58a0586bb91d996fc51fa0cd155c57e8cd
Scrabbone <57068110+Scrabbone@users.noreply.github.com>
2020-12-18 12:20:05 +0100

    Added Cookie API; Created login

---
+++ cookieScript.js
@@ -0,0 +1,28 @@
+function setCookie(cname, cvalue, exhours){
+	var d = new Date();
+	d.setTime(d.getTime() + (exhours*60*60*1000));
+	var expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=./";
+}
+
+function getCookie(cname){
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+const COOKIE_EXPIRE_HOURS = 1;
+
+// export const COOKIE_EXPIRE_HOURS = 1;
+
+// export {setCookie, getCookie, COOKIE_EXPIRE_HOURS};
---
+++ loginScript.js
@@ -0,0 +1,10 @@
+function login(ev){
+	// sessionStorage.setItem("name", document.getElementById("nameInput").value);
+	// sessionStorage.setItem("fach", document.getElementById("fachInput").value);
+	// sessionStorage.setItem("ort", document.getElementById("ortInput").value);
+	// sessionStorage.setItem("passwort", document.getElementById("passwordInput").value);
+	// sessionStorage.setItem("pic", profilePic);
+	// var pathname = window.location.pathname;
+	// window.location.replace(pathname.substring(0, pathname.lastIndexOf("Register.html")) + "Dashboard.html");
+	
+}
